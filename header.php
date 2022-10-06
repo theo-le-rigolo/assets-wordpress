8662032f9c1672c272224b3d5f57f980e4b4f0bf
Théo Feschotte <theofeschotte@gmail.com>
2022-10-06 12:15:22 +0200

    Create header.php

---
+++ header.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html class="no-js" <?php language_attributes(); ?>>
+    <head>
+        
+        <?php wp_head(); ?>
+        
+        <meta name="author" content="Studio 22">
+        <meta name="description" content="<?php bloginfo('description'); ?>">
+
+        <meta charset="<?php bloginfo('charset'); ?>">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1" />
+        
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        
+    </head>
+    <body data-mobile-nav-style="classic">
+        <!-- start header -->
+        <header>
+            <!-- start navigation -->
+            <nav class="navbar navbar-expand-lg navbar-dark bg-transparent header-light fixed-top header-reverse-scroll">
+                <div class="container-lg nav-header-container">
+                    <div class="col-auto col-sm-6 col-lg-2 me-auto ps-lg-0">
+                        <a class="navbar-brand" href="<?php echo get_home_url(); ?>" target="_self" rel="noopener"><img class="" src="#" alt="Logo de "></a>
+                    </div>
+                    <div class="col-auto menu-order px-lg-0">
+                        <button class="navbar-toggler float-end" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-line"></span>
+                            <span class="navbar-toggler-line"></span>
+                            <span class="navbar-toggler-line"></span>
+                            <span class="navbar-toggler-line"></span>
+                        </button>
+                        <!-- start menu header wordpress -->
+                        <?php wp_nav_menu(
+                            array(
+                                'menu'              => 'Menu header',
+                                'theme_location'    => 'menu-header',
+                                'depth'             => 0,
+                                'container'         => '',
+                                'container_id'      => '',
+                                'container_class'   => '',
+                                'menu_class'        => '',
+                                'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+                                'walker'            => new WP_Bootstrap_Navwalker(),
+                            )
+                        ); ?>
+                        <!-- end menu header wordpress -->
+                    </div>
+                </div>
+            </nav>
+            <!-- end navigation -->
+        </header>
+        <!-- end header -->
