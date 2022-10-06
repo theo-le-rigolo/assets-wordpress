42d9293add1de2f76288f68575628f09dc35535c
Théo Feschotte <theofeschotte@gmail.com>
2022-10-06 12:15:49 +0200

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,11 @@
+<!-- start footer -->
+<!-- end footer -->
+
+<!-- start scroll to top -->
+<a class="scroll-top-arrow bg-vert-hover text-extra-dark-gray-hover" href="javascript:void(0);"><i class="fas fa-arrow-up"></i></a>
+<!-- end scroll to top -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
