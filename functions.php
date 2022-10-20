52e27cc410eb0ebfc5b80110bb3a2efb9f1f67af
Théo Feschotte <theofeschotte@gmail.com>
2022-10-21 00:56:32 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -11,37 +11,37 @@ function ig_scripts() {
 	// Pour gérer les priorités d'appel CSS, ajouter l'ID dans les dépendances de l'appel du CSS principal
 
 	// Bootstrap CSS
-	wp_enqueue_style( 'ig_bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css', array(), IG_VERSION, 'all');
+	wp_enqueue_style( 'bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css', array(), IG_VERSION, 'all');
 
 	// <link rel="stylesheet" type="text/css" href="css/theme-vendors.min.css">
-	wp_enqueue_style( 'ig_themevendorthemecss', get_template_directory_uri().'/css/theme-vendors.min.css', array(), IG_VERSION, 'all');
+	wp_enqueue_style( 'themevendorthemecss', get_template_directory_uri().'/css/theme-vendors.min.css', array(), IG_VERSION, 'all');
 
 	// <link rel="stylesheet" type="text/css" href="css/style-theme.css" />
-	wp_enqueue_style( 'ig_stylethemecss', get_template_directory_uri().'/css/style-theme.css', array(), IG_VERSION, 'all');
+	wp_enqueue_style( 'stylethemecss', get_template_directory_uri().'/css/style-theme.css', array(), IG_VERSION, 'all');
 
 	// <link rel="stylesheet" type="text/css" href="css/responsive.css" />
-	wp_enqueue_style( 'ig_responsivethemecss', get_template_directory_uri().'/css/responsive.css', array(), IG_VERSION, 'all');
+	wp_enqueue_style( 'responsivethemecss', get_template_directory_uri().'/css/responsive.css', array(), IG_VERSION, 'all');
 
 	/* Feuille de styles personnalisée */
-	wp_enqueue_style( 'ig_customstyles', get_template_directory_uri().'/style.css', array(), IG_VERSION, 'all');
+	wp_enqueue_style( 'customstyles', get_template_directory_uri().'/style.css', array(), IG_VERSION, 'all');
 
 	// Font Awesome JS
-	wp_enqueue_script( 'ig_fontawesomejs', 'https://kit.fontawesome.com/cb8f0e6916.js', array(), IG_VERSION, true );
+	wp_enqueue_script( 'fontawesomejs', 'https://kit.fontawesome.com/cb8f0e6916.js', array(), IG_VERSION, true );
 	
 	// jQuery
 	// wp_enqueue_script('jquery');
 
 	// Bootstrap JS
-	wp_enqueue_script( 'ig_bootstrapjs', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js', array(), IG_VERSION, true );
+	wp_enqueue_script( 'bootstrapjs', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js', array(), IG_VERSION, true );
 
 	// <script type="text/javascript" src="js/jquery.min.js"></script>
-	wp_enqueue_script( 'ig_jquerrytheme', get_template_directory_uri().'/js/jquery.min.js', array(), IG_VERSION, true );
+	wp_enqueue_script( 'jquerytheme', get_template_directory_uri().'/js/jquery.min.js', array(), IG_VERSION, true );
 
 	// <script type="text/javascript" src="js/theme-vendors.min.js"></script>
-	wp_enqueue_script( 'ig_themevendorthemejs', get_template_directory_uri().'/js/theme-vendors.min.js', array(), IG_VERSION, true );
+	wp_enqueue_script( 'themevendorthemejs', get_template_directory_uri().'/js/theme-vendors.min.js', array(), IG_VERSION, true );
 
 	// <script type="text/javascript" src="js/main.js"></script>
-	wp_enqueue_script( 'ig_mainthemejs', get_template_directory_uri().'/js/main.js', array(), IG_VERSION, true );
+	wp_enqueue_script( 'mainthemejs', get_template_directory_uri().'/js/main.js', array(), IG_VERSION, true );
 	
 }
 add_action('wp_enqueue_scripts', 'ig_scripts');
